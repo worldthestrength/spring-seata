ec8216845517f5529e2f3f8c01c771b07c105197
yg.zh@outlook.com <93X2KqxUCkCZlCK0>
2022-06-21 15:08:12 +0800

    1

---
+++ src/test/java/com/example/springseata/SpringSeataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springseata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
